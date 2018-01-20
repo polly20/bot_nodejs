aca88d0f53511fa855b84283e00970664630eb9f
king052188 <me@kpa21.info>
2018-01-20 22:45:40 +0800

    1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser =  require('body-parser');
 var request = require('request');
+var cache = require('memory-cache');
 var app = express();
 app.use(bodyParser.urlencoded({"extended": false}));
 app.use(bodyParser.json());
@@ -8,9 +9,32 @@ app.use(bodyParser.json());
 const PAGE_ACCESS_TOKEN = "EAAYYmSZAZBhIkBACqwsWHRjX7P9ko35EPBZBiTnZAD2DlSBuZCtJJZByDHx1ZCMsyLFSEbocLP2ZCKQXZBVbOOphlmp8Hiv8RRounnGiVrAMcY6fvBhORI7H2HERJbeI31aTQ4KtSSioId02egzM5QSJOQ8nVXzHxCLxElpv1EU4oJwSMQDLAqUyA";
 const VERIFY_TOKEN = "PAOPAO2121";
 
+var newCache;
+
 // Test
 app.get('/test', (req, res) => {
-  res.send('Hello King');
+  callSendPtxt4wrdAPI(res, '09177715380', 'BAL');
+});
+
+// Test
+app.get('/mpin', (req, res) => {
+
+  var cache_mpin = newCache.get('MPIN');
+
+  var users_mpin = req.query["pin"]
+
+  console.log("mem_cache: " + cache_mpin);
+
+  console.log("user_pin: " + users_mpin);
+
+  res.setHeader("Content-Type", "text/html");
+
+  if(parseInt(cache_mpin) == parseInt(users_mpin)) {
+    res.send("nice");
+  }
+
+  res.send("nope");
+
 });
 
 // To verify
@@ -60,15 +84,38 @@ app.post('/webhook', (req, res) => {
 
 });
 
-
 function handleMessage(sender_psid, received_message) {
   let response;
   // Check if the message contains text
   if (received_message.text) {
     // Create the payload for a basic text message
-    response = {
-      "text": `You sent the message: "${received_message.text}". Thanks`
+    var data;
+    var msg = received_message.text;
+    if(msg.includes("LC")) {
+      data = msg.split("/");
+      console.log(data);
+      if(data.length > 1) {
+        callSendPtxt4wrdAPI(sender_psid, data[1], data[2]);
+      }
     }
+    else if(msg.includes("MPIN")) {
+      data = msg.split(" ");
+      console.log(data);
+      if(data.length > 0) {
+        mpinVerify(sender_psid, data[1]);
+      }
+    }
+    else {
+      handleMessageSend(sender_psid, msg);
+    }
+  }
+}
+
+function handleMessageSend(sender_psid, received_message) {
+  let response;
+  // Create the payload for a basic text message
+  response = {
+    "text": `${received_message}`
   }
   // Sends the response message
   callSendAPI(sender_psid, response);
@@ -114,4 +161,109 @@ function callSendAPI(sender_psid, response) {
   });
 }
 
-app.listen(3200);
+
+// ptxt4wrd 
+
+function callSendPtxt4wrdAPI(sender_psid, mobile, command) {
+  // Construct the message body
+
+  let request_body = {
+    "mobile": mobile,
+    "command": command
+  }
+
+  newCache = new cache.Cache();
+  var stringMSG = null;
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "http://localhost:8002/command/execute/v2",
+    "method": "GET",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+
+      var mpin_verify = body['MPIN'];
+      newCache.put('MPIN', mpin_verify);
+      newCache.put('MOBILE', mobile);
+      newCache.put('COMMAND', command);
+
+      var summary = "We have sent MPIN to this mobile (" + body['Mobile'] + "), ";
+      summary += "your command is (" + body['Command'] + "), Please type MPIN<space>6 digits then hit enter";
+
+      console.log(summary);
+      console.log(newCache.get('MPIN'));
+
+      handleMessageSend(sender_psid, summary);
+    } 
+    else {
+      console.error("Unable to send message:" + err);
+      stringMSG = "Unable to send message:" + err;
+
+      handleMessageSend(sender_psid, stringMSG);
+    }
+  });
+
+  return stringMSG;
+}
+
+function callSendLoad4wrdAPI(sender_psid, mobile, command) {
+  // Construct the message body
+
+  let request_body = {
+    "mobile": mobile,
+    "command": command
+  }
+
+  newCache = new cache.Cache();
+  var stringMSG = null;
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "http://localhost:8002/command/process/v2",
+    "method": "GET",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      var status = parseInt(body['Status']);
+      var msg = "Something went wrong, please try again.";
+      if(status == 200) {
+        msg = "Your request is being processed.";
+      }
+
+      console.error(msg);
+      handleMessageSend(sender_psid, msg);
+    } 
+    else {
+      console.error("Unable to send message:" + err);
+      stringMSG = "Unable to send message:" + err;
+
+      handleMessageSend(sender_psid, stringMSG);
+    }
+  });
+
+  return stringMSG;
+}
+
+function mpinVerify(sender_psid, users_mpin) {
+
+  var cache_mpin = 0;
+  try{
+    cache_mpin = newCache.get('MPIN');
+
+    cache_mobile = newCache.get('MOBILE');
+    cache_command = newCache.get('COMMAND');
+
+    if(parseInt(cache_mpin) == parseInt(users_mpin)) {
+      callSendLoad4wrdAPI(sender_psid, cache_mobile, cache_command);
+    }
+    else {
+      handleMessageSend(sender_psid, "Invalid MPIN");
+    }
+
+  } catch( err ){
+    handleMessageSend(sender_psid, "No MPIN set.");
+  }
+  console.log("mem_cache: " + cache_mpin);
+  console.log("user_pin: " + users_mpin);
+}
+
+app.listen(process.env.PORT);
